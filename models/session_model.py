b93d8b9171b91d15df2a95568acac8c615973f79
nik1tosssa <realprimestels@gmail.com>
2026-03-25 02:16:32 +0300

    feat: add Session model to handle keyword collection, brand comparison, and CSV generation

---
+++ models/session_model.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+import os
+from app.services import scraper
+from app.services.openai_srv import classify_queries
+from app.utils.url_generator import generate_url
+import pandas as pd
+
+
+@dataclass
+class Session:
+    # Для строк всё просто — пишем ""
+    start_keywords_file_path: str = ""
+    geo: str = "IT"
+    period: str = "today 3-m"
+    most_popular_keyword: str = ""
+    second_popular_keyword: str = ""
+
+    # Для списков используем default_factory
+    urls: list[str] = field(default_factory=list)
+    start_keywords: list[str] = field(default_factory=list)
+    full_scraped_keywords: list[str] = field(default_factory=list)
+    brand_keywords: list[str] = field(default_factory=list)
+    pairs_of_values: list[list[str]] = field(default_factory=list)
+
+    def collect_keywords(self):
+
+        for url in self.urls:
+            keys = scraper.get_new_keywords(url=url)
+            self.full_scraped_keywords = list(set(keys + self.full_scraped_keywords))
+
+    def collect_brand_keywords(self):
+        resp = classify_queries(self.full_scraped_keywords)
+        self.brand_keywords = list(set(
+            item['clean_brand'] for item in resp['result'] if item['clean_brand']
+        ))
+        # self.brand_keywords
+        print(self.brand_keywords)
+
+    def compare_brands(self):
+        for brand in self.brand_keywords:
+            url = generate_url(first_keyword=self.most_popular_keyword, second_keyword=brand, geo=self.geo.upper(),
+                               period=self.period)
+            print(f"Brand: {brand}, URL: {url}")
+            pair = scraper.get_first_pair_values(url)
+            print(pair)
+            self.pairs_of_values.append(pair)
+
+    def create_csv(self, doc_id: Optional[str] = "None"):
+        file_path = os.path.join("./output", f"output_{doc_id}.csv")
+        csv_data = []
+
+
+        for i in range(len(self.brand_keywords)):
+
+            try:
+                first_value = self.pairs_of_values[i][0]
+            except IndexError:
+                print(f"IndexError: {i}")
+                first_value = "no value"
+
+            try:
+                second_value = self.pairs_of_values[i][1]
+            except IndexError:
+                print(f"IndexError: {i}")
+                second_value = "no value"
+
+            csv_data.append({
+                "first_keyword": self.most_popular_keyword,
+                "brand_keyword": self.brand_keywords[i],
+                "first_value": first_value,
+                "second_value": second_value,
+            })
+        df = pd.DataFrame(csv_data, columns=["first_keyword", "brand_keyword", "first_value", "second_value"])
+        df.to_csv(file_path, index=False, encoding='utf-8-sig', sep=';')
+
+        return file_path
