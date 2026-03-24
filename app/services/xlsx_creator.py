f0ea17da3dcc99a53625942b02a61590a543ca10
nik1tosssa <realprimestels@gmail.com>
2026-03-25 02:16:20 +0300

    refactor: switch XLSX output to CSV format and integrate query classification with OpenAI

--- app/services/xlsx_creator.py
+++ app/services/xlsx_creator.py
@@ -1,10 +1,11 @@
 import app.services.scraper as scraper
 import os
 import pandas as pd
+from app.services.openai_srv import classify_queries
 
 
 def get_new_xlsx_file_path(rows: list, id: str) -> str:
-    file_path = os.path.join("readies", f"data_{id}.xlsx")
+    file_path = os.path.join("output", f"data_{id}.csv")
     all_pairs_data = []  # Список для накопления данных всех пар
 
     for row in rows:
@@ -20,6 +21,8 @@ def get_new_xlsx_file_path(rows: list, id: str) -> str:
                 pair_of_values = 'err'
             if len(scraped_data[1]) > 0:
                 keywords = scraped_data[1]
+                gpt_result = classify_queries(keywords)
+                print(gpt_result)
             else:
                 keywords = 'err'
         else:
@@ -35,9 +38,10 @@ def get_new_xlsx_file_path(rows: list, id: str) -> str:
 
     # Сохраняем всё сразу после цикла
     df = pd.DataFrame(all_pairs_data)
+    df.to_csv(file_path, index=False, encoding='utf-8-sig', sep=';')
     # Вместо df.to_excel(file_path)
-    with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-        df.to_excel(writer, index=False)
+    # with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+    #     df.to_excel(writer, index=False)
     # В этой точке файл уже точно должен быть на диске
 
     return file_path
