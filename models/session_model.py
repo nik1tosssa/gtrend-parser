131fa70df8c34ec1b6039def91142a973b86fb84
nikitabozko <realprimestels@gmail.com>
2026-03-26 09:22:38 +0300

    Refactor `collect_keywords_and_value_pairs` to simplify loop logic and adjust URL generation.

--- models/session_model.py
+++ models/session_model.py
@@ -29,53 +29,52 @@ class Session:
     csv_data: list[dict] = field(default_factory=list)
 
     def collect_keywords_and_value_pairs(self):
-        count = (len(self.start_keywords) * len(self.start_keywords) - 1) / 2
+        count = len(self.start_keywords)
         counter = 0
-        for i in range(len(self.start_keywords)):
-            for j in range(i + 1, len(self.start_keywords)):
-                url = generate_url(first_keyword=self.start_keywords[i], second_keyword=self.start_keywords[j],
-                                   geo=self.geo.upper(), period=self.period)
+        for key in self.start_keywords:
+            url = generate_url(first_keyword=self.most_popular_keyword, second_keyword=self.key,
+                               geo=self.geo.upper(), period=self.period)
 
-                trends_data = scraper.get_google_trends_data(url=url)
-                try:
-                    keys = trends_data[1]
-                except IndexError:
-                    keys = []
+            trends_data = scraper.get_google_trends_data(url=url)
+            try:
+                keys = trends_data[1]
+            except IndexError:
+                keys = []
 
-                self.full_scraped_keywords = list(set(keys + self.full_scraped_keywords))
-                coef = 0
-                coef_1 = 0
-                coef_2 = 0
-                try:
-                    self.pairs_of_values.append(trends_data[0])
-                except IndexError:
-                    self.pairs_of_values.append(["0", "0"])
-                try:
-                    first_value = int(self.pairs_of_values[i][0])
-                    if first_value != 0:
-                        coef = 100 / first_value
-                    else:
-                        coef = 9999999
-                    coef_1 = 100
-                except IndexError:
-                    first_value = "0"
+            self.full_scraped_keywords = list(set(keys + self.full_scraped_keywords))
+            coef = 0
+            coef_1 = 0
+            coef_2 = 0
+            try:
+                self.pairs_of_values.append(trends_data[0])
+            except IndexError:
+                self.pairs_of_values.append(["0", "0"])
+            try:
+                first_value = int(self.pairs_of_values[i][0])
+                if first_value != 0:
+                    coef = 100 / first_value
+                else:
+                    coef = 9999999
+                coef_1 = 100
+            except IndexError:
+                first_value = "0"
 
-                try:
-                    second_value = int(self.pairs_of_values[i][1])
-                    coef_2 = second_value * coef
-                except IndexError:
-                    second_value = "0"
+            try:
+                second_value = int(self.pairs_of_values[i][1])
+                coef_2 = second_value * coef
+            except IndexError:
+                second_value = "0"
 
-                self.csv_data.append({
-                    "first_keyword": self.start_keywords[i],
-                    "brand_keyword": self.start_keywords[j],
-                    "first_value": first_value,
-                    "second_value": second_value,
-                    "coef_1": coef_1,
-                    "coef_2": coef_2,
-                })
-                counter += 1
-                logging.info(f"Progress: {counter}/{count}")
+            self.csv_data.append({
+                "first_keyword": self.start_keywords[i],
+                "brand_keyword": self.start_keywords[j],
+                "first_value": first_value,
+                "second_value": second_value,
+                "coef_1": coef_1,
+                "coef_2": coef_2,
+            })
+            counter += 1
+            logging.info(f"Progress: {counter}/{count}")
 
         # for url in self.urls:
         #     keys = scraper.get_new_keywords(url=url)
